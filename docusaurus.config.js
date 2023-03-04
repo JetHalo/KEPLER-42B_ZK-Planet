fdc56467754d0cd37e5847ef1e2259dcd7e0d840
lovingzizi <turingjohn2015@gmail.com>
2023-03-04 21:07:35 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'ZK Planet',
+  tagline: 'Pink Dinosaur',
+  url: 'https://zkdoc.kepler42b.xyz/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
